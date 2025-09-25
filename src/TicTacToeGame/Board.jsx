1125e85128127884fe5f9c350b63e43c21abeb24
Shahid <23it09.arman@pccegoa.edu.in>
2025-09-25 21:25:19 +0530

    Updated square size and styling

--- src/TicTacToeGame/Board.jsx
+++ src/TicTacToeGame/Board.jsx
@@ -1,73 +1,89 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
-    const [state, setState] = useState(Array(9).fill(null));
-    const [isXTurn, setIsXTurn] = useState(true);
+  const [state, setState] = useState(Array(9).fill(null));
+  const [isXTurn, setIsXTurn] = useState(true);
 
-    const checkWinner = () => {
-        const winnerLogic = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
+  const checkWinner = () => {
+    const winnerLogic = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
 
-        for(let logic of winnerLogic) {
-            const [a, b, c] = logic;
-            if(state[a] !== null && state[a] === state[b] && state[a] === state[c]){
-                return state[a];
-            }
-        }
-        return false;
-    };
-
-    const isWinner = checkWinner();
-
-const handleClick = (index) => {
-    if (state[index] !== null) {
-        return;
+    for (let logic of winnerLogic) {
+      const [a, b, c] = logic;
+      if (state[a] !== null && state[a] === state[b] && state[a] === state[c]) {
+        return state[a];
+      }
     }
+    return false;
+  };
+
+  const handleClick = (index) => {
+    if (state[index] !== null) return; // stop if already filled
     const copyState = [...state];
     copyState[index] = isXTurn ? "X" : "O";
     setState(copyState);
     setIsXTurn(!isXTurn);
-};
+  };
 
-const handleReset =() => {
-    setState(Array(9).fill(null));
-}
+  const winner = checkWinner();
 
-    return (
-        <div className="board-container">
-            {isWinner ? (
-                <>{isWinner} won the game <button onClick={handleReset}>Play Again</button></>
-            ) : (
-                <>
-                <h4>Player { isXTurn ? "X" : "O"} please move</h4>
-            <div className="board-row">
-                <Square onClick={() => handleClick(0)} value={state[0]}/>
-                <Square onClick={() => handleClick(1)} value={state[1]}/>
-                <Square onClick={() => handleClick(2)} value={state[2]}/>
-            </div>
-            <div className="board-row">
-                <Square onClick={() => handleClick(3)} value={state[3]}/>
-                <Square onClick={() => handleClick(4)} value={state[4]}/>
-                <Square onClick={() => handleClick(5)} value={state[5]}/>
-            </div>
-            <div className="board-row">
-                <Square onClick={() => handleClick(6)} value={state[6]}/>
-                <Square onClick={() => handleClick(7)} value={state[7]}/>
-                <Square onClick={() => handleClick(8)} value={state[8]}/>
-            </div>
-        </>
-            )}
-            </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        height: "100vh"
+      }}
+    >
+      {/* Message */}
+      <h3>
+        {winner
+          ? `Winner is ${winner}`
+          : `Player ${isXTurn ? "X" : "O"} please move`}
+      </h3>
+
+      {/* Board grid */}
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 100px)",
+          gridTemplateRows: "repeat(3, 100px)",
+          marginTop: "20px"
+        }}
+      >
+        {state.map((val, i) => (
+          <Square key={i} value={val} onClick={() => handleClick(i)} />
+        ))}
+      </div>
+
+      {/* Restart button */}
+      <button
+        onClick={() => {
+          setState(Array(9).fill(null)); // reset board
+          setIsXTurn(true);              // reset turn
+        }}
+        style={{
+          marginTop: "20px",
+          padding: "10px 20px",
+          fontSize: "16px",
+          cursor: "pointer"
+        }}
+      >
+        Restart Game
+      </button>
+    </div>
+  );
 };
 
 export default Board;
--- src/TicTacToeGame/Square.jsx
+++ src/TicTacToeGame/Square.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const Square = (props) => {
-    return (
-        <div
-        onClick={props.onClick}
-         style={{ border: "1px solid", height: "100px", width: "50%" , display:"flex", justifyContent:"center", alignItems: "center"}}
-        className="Square">
-            <h5>{props.value}</h5>
-        </div>
-    )
+  return (
+    <div
+      onClick={props.onClick}
+      style={{
+        border: "1px solid black",
+        height: "100px",
+        width: "100px",   // fixed width
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "2rem",
+        cursor: "pointer",
+      }}
+      className="square"
+    >
+      <h5>{props.value}</h5>
+    </div>
+  );
 };
 
-export default Square; 
+export default Square;
